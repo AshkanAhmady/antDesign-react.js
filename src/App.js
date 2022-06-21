623f8eeed4b567fcd1ffa662be3314fd5018803d
AshkanAhmadi <ashkan16ma96@gmail.com>
2022-06-21 16:44:49 +0430

    add (layout)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
 import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import React, { useState } from "react";
+import {
   DatePicker as DatePickerJalali,
   Calendar,
   JalaliLocaleListener,
 } from "antd-jalali";
 
+const { Header, Sider, Content } = Layout;
+
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div className="App">
-      <JalaliLocaleListener />
-      تاریخ: <DatePickerJalali />
-      {/* رنج تاریخ: <DatePickerJalali.RangePicker />
-      <Calendar /> */}
-    </div>
+    <Layout className="layout">
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">
+          <h4>LOGO</h4>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "nav 1",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
 export default App;
+
+{
+  /* <div className="App">
+        <JalaliLocaleListener />
+        انتخاب تاریخ: <DatePickerJalali />
+        Jalali RangePicker: <DatePickerJalali.RangePicker />
+        <br />
+        <br />
+        <Calendar />
+      </div> */
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import "antd/dist/antd.css";
+import "./index.css";
 import { ConfigProvider } from "antd";
 import fa_IR from "antd/lib/locale/fa_IR";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ConfigProvider locale={fa_IR} direction="rtl">
+  <ConfigProvider locale={fa_IR}>
     <App />
   </ConfigProvider>
 );
